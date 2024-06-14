14220ecfe8664fd03fc8f9c7a299afbb2773fcbb
Dustin Michels <dustin7538@gmail.com>
2024-06-14 11:31:52 -0400

    implement modal image popup

---
+++ static/js/modal.js
@@ -0,0 +1,31 @@
+function openModal(imageId) {
+  console.log('clicked')
+
+  const modal = document.getElementById('myModal')
+  if (modal === null) {
+    return
+  }
+  // Get the original image element and use it inside the modal
+  const img = document.getElementById(imageId)
+  const modalImg = document.getElementById('modalImg')
+  const captionText = document.getElementById('caption')
+
+  // Display modal
+  modal.style.display = 'block'
+  modalImg.src = img.src
+  captionText.innerHTML = img.alt
+
+  // The element that closes the modal
+  const close = document.getElementsByClassName('modal-close')[0]
+
+  // When the user clicks on (x) button, close modal.
+  close.onclick = function () {
+    modal.style.display = 'none'
+  }
+  // When the user hits ESC, close modal as well.
+  document.addEventListener('keydown', (event) => {
+    if (event.key === 'Escape') {
+      modal.style.display = 'none'
+    }
+  })
+}
